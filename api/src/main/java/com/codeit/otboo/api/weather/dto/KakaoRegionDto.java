69abae8fd0af11c0ac287289245666779970580d
kyhun1007 <kyhun1007@naver.com>
2026-09-07 10:58:06 +0900

    feat: 외부 API 클라이언트 및 설정 코드 추가

---
+++ api/src/main/java/com/codeit/otboo/api/weather/dto/KakaoRegionDto.java
@@ -0,0 +1,13 @@
+package com.codeit.otboo.api.weather.dto;
+
+public record KakaoRegionDto(
+	String regionType,
+	String code,
+	String addressName,
+	String region1DepthName,
+	String region2DepthName,
+	String region3DepthName,
+	String region4DepthName,
+	double longitude,
+	double latitude
+) {}
