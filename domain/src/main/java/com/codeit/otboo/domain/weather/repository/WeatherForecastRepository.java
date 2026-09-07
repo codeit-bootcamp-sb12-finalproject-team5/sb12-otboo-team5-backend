1d9f3bb9f2aceeebec2b4e3d213d6bde7c9671de
kyhun1007 <kyhun1007@naver.com>
2026-09-07 16:11:09 +0900

    merge: dev 브랜치 flyway 도입 변경사항 통합

--- domain/src/main/java/com/codeit/otboo/domain/weather/repository/WeatherForecastRepository.java
+++ domain/src/main/java/com/codeit/otboo/domain/weather/repository/WeatherForecastRepository.java
@@ -3,10 +3,8 @@ package com.codeit.otboo.domain.weather.repository;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.codeit.otboo.domain.weather.entity.WeatherForecast;
 
-@Repository
 public interface WeatherForecastRepository extends JpaRepository<WeatherForecast, UUID> {
 }
