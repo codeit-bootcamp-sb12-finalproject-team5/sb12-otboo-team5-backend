6a688b7d6622b68df34bdcab2ee2f227a6358244
Jiye-Lee <cjstk9081@gmail.com>
2026-09-11 18:25:54 +0900

    feat: 프로필 예외 처리 추가

---
+++ domain/src/main/java/com/codeit/otboo/domain/profile/exception/ProfileException.java
@@ -0,0 +1,19 @@
+package com.codeit.otboo.domain.profile.exception;
+
+import com.codeit.otboo.domain.common.exception.BusinessException;
+import com.codeit.otboo.domain.common.exception.ErrorCode;
+
+public class ProfileException extends BusinessException {
+
+  public ProfileException(ErrorCode errorCode) {
+    super(errorCode);
+  }
+
+  public ProfileException(ErrorCode errorCode, Throwable cause) {
+    super(errorCode, cause);
+  }
+
+  public static ProfileException notFound() {
+    return new ProfileException(ErrorCode.PROFILE_NOT_FOUND);
+  }
+}
