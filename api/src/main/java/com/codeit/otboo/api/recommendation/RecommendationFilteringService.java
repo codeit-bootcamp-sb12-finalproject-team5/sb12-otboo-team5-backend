ba738e0cb27f0a41f336eb2061c8cb96aedcbf89
99hyeon <hyeon99@ajou.ac.kr>
2026-09-16 10:07:26 +0900

    setting: webclient 의존성 추가 및 gemini 연동 세팅(#KAN-114)

--- api/src/main/java/com/codeit/otboo/api/recommendation/RecommendationFilteringService.java
+++ api/src/main/java/com/codeit/otboo/api/recommendation/RecommendationFilteringService.java
@@ -4,13 +4,13 @@ import com.codeit.otboo.api.recommendation.candidate.ClothesCandidateProvider;
 import com.codeit.otboo.api.recommendation.filter.ClothesFilteringContext;
 import com.codeit.otboo.api.recommendation.filter.RuleBasedClothesFilter;
 import com.codeit.otboo.api.recommendation.temperature.TemperatureAdjustmentPolicy;
-import com.codeit.otboo.api.weather.exception.WeatherException;
 import com.codeit.otboo.domain.clothes.entity.Clothes;
 import com.codeit.otboo.domain.common.exception.ErrorCode;
 import com.codeit.otboo.domain.profile.entity.Profile;
 import com.codeit.otboo.domain.profile.exception.ProfileException;
 import com.codeit.otboo.domain.profile.repository.ProfileRepository;
 import com.codeit.otboo.domain.weather.entity.WeatherForecast;
+import com.codeit.otboo.domain.weather.exception.WeatherException;
 import com.codeit.otboo.domain.weather.repository.WeatherForecastRepository;
 
 import java.math.BigDecimal;
