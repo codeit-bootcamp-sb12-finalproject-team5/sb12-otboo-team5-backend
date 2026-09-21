1ef8ce3388ade4ceb8888bb7e98f050e421c99e9
kyhun1007 <kyhun1007@naver.com>
2026-09-21 11:35:59 +0900

    refactor: 하늘상태 및 예측정보 Enum 추가에 따른 응답 Dto 내 타입 조정

--- api/src/main/java/com/codeit/otboo/api/weather/dto/response/WeatherViewData.java
+++ api/src/main/java/com/codeit/otboo/api/weather/dto/response/WeatherViewData.java
@@ -1,5 +1,6 @@
 package com.codeit.otboo.api.weather.dto.response;
 
+import com.codeit.otboo.domain.weather.entity.SkyStatus;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
@@ -7,7 +8,7 @@ public record WeatherViewData(
     UUID id,
     LocalDateTime forecastedAt,
     LocalDateTime forecastAt,
-    String skyStatus,
+    SkyStatus skyStatus,
     PrecipitationDto precipitation,
     HumidityDto humidity,
     TemperatureDto temperature,
